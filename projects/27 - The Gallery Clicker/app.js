e347bd9150c506842da414e0fe74b34317cdf59d
Brian Bycynski <high5bri@hotmail.com>
2021-05-25 02:29:41 -0400

    finished gallery clicker project

--- projects/27 - The Gallery Clicker/app.js
+++ projects/27 - The Gallery Clicker/app.js
@@ -1,2 +1,16 @@
 //
 //
+
+const currentImg = document.querySelector('.current-img');
+const imgs = document.querySelectorAll('.all-imgs img');
+
+imgs.forEach(function (img) {
+  img.addEventListener('click', function (e) {
+    currentImg.src = e.target.src;
+    currentImg.classList.add('animate-imgs');
+
+    setTimeout(function () {
+      currentImg.classList.remove('animate-imgs');
+    }, 400);
+  });
+});
