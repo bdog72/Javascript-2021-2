772b8ca550727ef90e04eb2eebc67f051afe4ac1
Brian Bycynski <high5bri@hotmail.com>
2021-05-28 02:15:27 -0400

    finished form validator

--- projects/28 - Form Validator/app.js
+++ projects/28 - Form Validator/app.js
@@ -1,2 +1,33 @@
 //
 //
+
+const usernameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#password');
+
+const formInputdValidator = /^(?=\D{8})(?=\D*\d{2})/;
+
+usernameInput.addEventListener('input', validate);
+passwordInput.addEventListener('input', validate);
+
+function validate(e) {
+  if (e.target.id === 'username') {
+    if (e.target.value.length > 3) {
+      e.target.classList.add('valid');
+      e.target.classList.remove('invalid');
+    } else {
+      e.target.classList.add('invalid');
+      e.target.classList.remove('valid');
+    }
+  }
+
+  if (e.target.id === 'password') {
+    if (formInputdValidator.test(e.target.value)) {
+      e.target.classList.add('valid');
+      e.target.classList.remove('invalid');
+    } else {
+      e.target.classList.add('invalid');
+      e.target.classList.remove('valid');
+    }
+    console.log(formInputdValidator.test(e.target.value));
+  }
+}
